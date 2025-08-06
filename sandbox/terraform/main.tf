7578d68cb3e77cb945ab3db010e523ec26253d57
raman sharma <raman.sharma@msn.com>
2025-08-06 13:09:45 +0100

    test implementation

--- sandbox/terraform/main.tf
+++ sandbox/terraform/main.tf
@@ -1,5 +1,19 @@
-resource "null_resource" "test_customization" {
-  provisioner "local-exec" {
-    command = "echo 'Customization pipeline is triggered'"
+provider "aws" {
+  region = "eu-west-2"
+}
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_s3_bucket" "sandbox_bucket" {
+  bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
+
+  tags = {
+    Name        = "AFT Sandbox Bucket"
+    Environment = "sandbox"
+    ManagedBy   = "AFT"
+  }
+
+  lifecycle {
+    prevent_destroy = true
   }
 }
--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -1,15 +1,15 @@
-provider "aws" {
-  region = "eu-west-2"  # or any other appropriate region
-}
+# provider "aws" {
+#   region = "eu-west-2"  # or any other appropriate region
+# }
 
-data "aws_caller_identity" "current" {}
+# data "aws_caller_identity" "current" {}
 
-resource "aws_s3_bucket" "sandbox_bucket" {
-  bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
+# resource "aws_s3_bucket" "sandbox_bucket" {
+#   bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
 
-  tags = {
-    Name        = "AFT Sandbox Bucket"
-    Environment = "sandbox"
-    ManagedBy   = "AFT"
-  }
-}
+#   tags = {
+#     Name        = "AFT Sandbox Bucket"
+#     Environment = "sandbox"
+#     ManagedBy   = "AFT"
+#   }
+# }
